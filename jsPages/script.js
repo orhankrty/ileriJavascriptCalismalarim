431dc65fd9bf1bfbf5954c0d7fa0d94e6b7531aa
orhan <hsueyinorhankirtay55@gmail.com>
2026-03-31 23:49:44 +0300

    document nesnesi ve özellikleri kodlandı

---
+++ jsPages/script.js
@@ -0,0 +1,7 @@
+console.log(document);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.body);
+console.log(document.head);
+console.log(document.forms);
+console.log(document.scripts);
